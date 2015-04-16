72562e1aad5ecec21bbf0421eef703cabc612f73
Daniel Buckmaster <dan.buckmaster@gmail.com>
2015-04-17 09:03:59 +1000

    Modified style without changing semantics.
     * Reordered code so main comes first
     * 4 space indentation
     * Print results so I can see progress T_T
     * Qualify CSV import
     * Swing data definition instead of using unswung style

--- haskell/haskell-k-nn.hs
+++ haskell/haskell-k-nn.hs
@@ -1,24 +1,41 @@
 {-# LANGUAGE BangPatterns #-}
 
-import Data.Csv
+import qualified Data.Csv as CSV
 import qualified Data.ByteString.Lazy as BS
 import qualified Data.Vector as V
 
-type Label = Field
+main = do
+    validationSample <- fmap parseRecords (BS.readFile "validationsample.csv")
+    trainingSample   <- fmap parseRecords (BS.readFile "trainingsample.csv")
 
-type Feature = Integer
+    case (validationSample, trainingSample) of
+        (Right validation, Right training) -> runClassifier validation training
+        _ -> putStrLn "Parsing error"
 
-data Observation = Observation { label :: !Label
-                               , features :: !(V.Vector Feature)
-                               } deriving (Show, Eq)
-
-instance FromRecord Observation where
-  parseRecord !v = do
-    V.mapM parseField (V.tail v) >>=
-      return . Observation (V.head v)
+runClassifier :: (V.Vector Observation) -> (V.Vector Observation) -> IO ()
+runClassifier validation training = do
+    let n = V.length validation
+    results <- V.forM validation $ \v -> do
+        let c = checkCorrect training v
+        print (label v, c)
+        return c
+    print (fromIntegral (V.sum results) / fromIntegral n)
 
 parseRecords :: BS.ByteString -> Either String (V.Vector Observation)
-parseRecords = decode HasHeader
+parseRecords = CSV.decode CSV.HasHeader
+
+data Observation = Observation {
+    label    :: !Label,
+    features :: !(V.Vector Feature)
+ } deriving (Show, Eq)
+
+type Label = CSV.Field
+type Feature = Integer
+
+instance CSV.FromRecord Observation where
+    parseRecord !v = do
+        pixels <- V.mapM CSV.parseField (V.tail v)
+        return $ Observation (V.head v) pixels
 
 dist :: (V.Vector Feature) -> (V.Vector Feature) -> Integer
 dist !x !y = V.sum $ V.map (^2) $ V.zipWith (-) x y
@@ -28,18 +45,9 @@ closerTo !target !o1 !o2 = compare (dist target (features o1)) (dist target (fea
 
 classify :: (V.Vector Observation) -> (V.Vector Feature) -> Label
 classify !os !fs = label where
-  (Observation label _) = V.minimumBy (closerTo fs) os
+    (Observation label _) = V.minimumBy (closerTo fs) os
 
 checkCorrect :: (V.Vector Observation) -> Observation -> Int
 checkCorrect !training (Observation label features)
-  | label == classify training features = 1
-  | otherwise = 0
-
-main = do
-  validationSample <- BS.readFile "validationsample.csv"
-  trainingSample <- BS.readFile "trainingsample.csv"
-
-  let Right validation = parseRecords validationSample
-  let Right training = parseRecords trainingSample
-  
-  print (V.sum (V.map (checkCorrect training) validation))
+    | label == classify training features = 1
+    | otherwise = 0
