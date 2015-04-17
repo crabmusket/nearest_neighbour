479ba4075ec6b63be74681544728d12dcbefaaf3
Daniel Buckmaster <dan.buckmaster@gmail.com>
2015-04-17 13:59:04 +1000

    Use unboxed vectors of features.

--- haskell/haskell-k-nn.hs
+++ haskell/haskell-k-nn.hs
@@ -3,6 +3,7 @@
 import qualified Data.Csv as CSV
 import qualified Data.ByteString.Lazy as BS
 import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as VU
 
 main = do
     validationSample <- fmap parseRecords (BS.readFile "validationsample.csv")
@@ -12,7 +13,7 @@ main = do
         (Right validation, Right training) -> runClassifier validation training
         _ -> putStrLn "Parsing error"
 
-runClassifier :: (V.Vector Observation) -> (V.Vector Observation) -> IO ()
+runClassifier :: Observations -> Observations -> IO ()
 runClassifier validation training = do
     let n = V.length validation
     results <- V.forM validation $ \v -> do
@@ -26,28 +27,29 @@ parseRecords = CSV.decode CSV.HasHeader
 
 data Observation = Observation {
     label    :: !Label,
-    features :: !(V.Vector Feature)
+    features :: !Features
  } deriving (Show, Eq)
 
 type Label = CSV.Field
-type Feature = Integer
+type Features = VU.Vector Int
+type Observations = V.Vector Observation
 
 instance CSV.FromRecord Observation where
     parseRecord !v = do
         pixels <- V.mapM CSV.parseField (V.tail v)
-        return $ Observation (V.head v) pixels
+        return $ Observation (V.head v) (VU.convert pixels)
 
-dist :: (V.Vector Feature) -> (V.Vector Feature) -> Integer
-dist !x !y = V.sum $ V.map (^2) $ V.zipWith (-) x y
+dist :: Features -> Features -> Int
+dist !x !y = VU.sum $ VU.map (^2) $ VU.zipWith (-) x y
 
-closerTo :: (V.Vector Feature) -> Observation -> Observation -> Ordering
+closerTo :: Features -> Observation -> Observation -> Ordering
 closerTo !target !o1 !o2 = compare (dist target (features o1)) (dist target (features o2))
 
-classify :: (V.Vector Observation) -> (V.Vector Feature) -> Label
+classify :: Observations -> Features -> Label
 classify !os !fs = label where
     (Observation label _) = V.minimumBy (closerTo fs) os
 
-checkCorrect :: (V.Vector Observation) -> Observation -> Int
+checkCorrect :: Observations -> Observation -> Int
 checkCorrect !training (Observation label features)
     | label == classify training features = 1
     | otherwise = 0
